08ea6f56fc8feae7a619ef894d444dc4a32c6d15
newt239 <ysti2019.win2020.st@gmail.com>
2022-04-01 20:24:06 +0900

    add: search page

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import { Router, Route } from "svelte-routing";
   import Home from "./pages/Home.svelte";
+  import Search from "./pages/Search.svelte";
 </script>
 
 <main>
   <Router>
     <Route path="" component={Home} name="world" />
+    <Route path="search" component={Search} />
   </Router>
 </main>
---
+++ src/pages/Search.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  let apiKey: string = "";
+  let url: string = "";
+  let result = [];
+</script>
+
+<main>
+  <h1>Search Youtube movies</h1>
+  <h2>api key</h2>
+  <input type="text" bind:value={apiKey} />
+  <h2>url</h2>
+  <input type="url" bind:value={url} />
+  <input type="button" />
+  <div>{result}</div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
